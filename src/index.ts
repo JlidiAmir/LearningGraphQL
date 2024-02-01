a998df0187c759a0853dd1838f6cf550d7dbdbe7
JlidiAmir <jlidiamir@gmail.com>
2024-02-01 15:38:32 +0100

    refactored index file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+
 // A schema is  a collection of type definitions ( hence "typeDefs" )
 // that together define the "shape" of queries that are excuted against your data.
 
