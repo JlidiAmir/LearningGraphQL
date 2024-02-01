6989c33c729d4d0a22435f4739f4c85a2da24487
JlidiAmir <jlidiamir@gmail.com>
2024-02-01 07:34:21 +0100

    added a WebSocket for subscription endpoint and used httpServer to handle both ws and express server

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ import { makeExecutableSchema } from "@graphql-tools/schema";
 import { WebSocketServer } from "ws";
 import { useServer } from "graphql-ws/lib/use/ws";
 
+
+
 // A schema is  a collection of type definitions ( hence "typeDefs" )
 // that together define the "shape" of queries that are excuted against your data.
 
@@ -37,7 +39,13 @@ type Query {
 type Mutation {
     addBook(title: String, author: String): [Book]
 }
+
+# The "Subscription" type is special
+type Subscription{
+    hello: String!
+}
 `;
+
 
 const books =[
     {
@@ -53,6 +61,7 @@ const books =[
         },
     },
 ];
+
 const authors =[
     {
         name: "Robert Kyosaki",
@@ -63,10 +72,11 @@ const authors =[
     }
 ]
 
+
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above. 
 
-const resolvers = {
+export const resolvers = {
     Query: {
         books: () => books,
         authors: ()=> authors,
@@ -77,19 +87,66 @@ const resolvers = {
             books.push({title:args.title,author:{name:args.author}});
             return books;
         }
+    },
+    Subscription: {
+        hello: {
+            // Example using an async generator
+            subscribe: async function* () 
+            {
+             for await (const word of ["Hello", "Bonjour", "Ciao"])
+             {
+                console.log(word);
+                yield { hello: word };
+             }   
+            },
+        },
+        // postCreated: {
+        //     // pub sub
+        //     subscribe: () => pubsub.asyncIterator(["POST_CREATED"]),
+        // }
+
     }
 };
 
-// The ApolloServer constructor requires two parameters: your schema definition and your resolvers
+// Creating an executable Schema object using the typeDefs and resolvers 
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+    
+const app = express(); 
+
+// Creating an http Server to wrap woth express and subscription server (WebSocket) and become our listener.
+const httpServer = createServer(app);
+
+// Creating the WebSocket server 
+const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/subscription"
+})
+
+// Passing the executable schema that we created to the WebSocket. 
+const serverCleanup = useServer({ schema }, wsServer);
+
+// The ApolloServer 
 
 const server = new ApolloServer<Context>(
     {
-        typeDefs, 
-        resolvers,
+        schema,
+        plugins: [
+        // Proper shutdown for the HTTP server.
+        ApolloServerPluginDrainHttpServer({httpServer}),
+        // Proper shutdown for the WebSocket server.
+        {
+            async serverWillStart(){
+                return {
+                    async drainServer(){
+                        await serverCleanup.dispose();
+                    }
+                }
+            }
+        }
+
+        ]
     }
 );
-
-const app = express(); 
 
 // Note you must call 'start()' on the 'ApolloServer'
 // instance before passing the instance to `expressMiddleware`
@@ -98,11 +155,14 @@ await server.start();
 
 // Specify the path where we'd like to mount our server
 
-app.use('/graphql', cors<cors.CorsRequest>(), express.json(), expressMiddleware(server));
+app.use('/subscription', cors<cors.CorsRequest>(), express.json(), expressMiddleware(server));
 
-app.listen(4000,()=>{
-    console.log("🚀 Server ready at http://localhost:4000/");
-})
+// app.listen(4000,()=>{
+//     console.log("🚀 Server ready at http://localhost:4000/");
+// })
+
+httpServer.listen(4000)
+console.log("🚀 Server ready at http://localhost:4000/");
 
 
 
